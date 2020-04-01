4b133a79db0fdd56f14a901efba172858055983e
Eric Abis <ericabis@Erics-MBP-2.fios-router.home>
2020-04-01 12:36:45 -0400

    minor uncommenting

--- etl_src/ETLs/IEEE/generate_schema.py
+++ etl_src/ETLs/IEEE/generate_schema.py
@@ -9,18 +9,18 @@ from etl_src.util.schema_inferences import SchemaInferences
 
 logger = init_logging("test_schema_inferences")
 
-# df = pd.read_csv("/app/data/train_transaction.csv")
-# logger.info("Read DF")
-# schema_inferences = SchemaInferences(df, logger)
-# converted_data_types = schema_inferences.create_converted_data_types()
+df = pd.read_csv("/app/data/train_transaction.csv")
+logger.info("Read DF")
+schema_inferences = SchemaInferences(df, logger)
+converted_data_types = schema_inferences.create_converted_data_types()
 
-# logger.info("transaction_python")
-# for col, dtype in schema_inferences.python_data_types.items():
-#     print('"{col}": {dtype},'.format(col = col, dtype = dtype))
+logger.info("transaction_python")
+for col, dtype in schema_inferences.python_data_types.items():
+    print('"{col}": {dtype},'.format(col = col, dtype = dtype))
 
-# logger.info("transaction_post_gres")
-# for col, dtype in schema_inferences.postgres_data_types.items():
-#     print('"{col}" {dtype},'.format(col = col, dtype = dtype))
+logger.info("transaction_post_gres")
+for col, dtype in schema_inferences.postgres_data_types.items():
+    print('"{col}" {dtype},'.format(col = col, dtype = dtype))
 
 df = pd.read_csv("/app/data/train_identity.csv")
 logger.info("Read DF")
--- etl_src/util/README.md
+++ etl_src/util/README.md
@@ -1,11 +1,11 @@
 ## schema_inferences class
 
-Seeing as there are 400 input columns I wrote this class a quick way of getting the schemas and creating the tables
+Seeing as there are around 400 input columns I wrote this class as a quick way of getting the schemas and creating the tables.  
 Since this is just a pretend exercise I'm letting Pandas do the work for me here.
 
 If this was a real ETL schema state could be maintained and enforced by either:
 
-1) tracking internally to the ETL application.  Ex:
+1) tracking internally to the ETL application, which is what I implemented in the schemas folder.  Ex:
 ``` python
 {table_1: {col_1: str,
           col_2: Decimal,
